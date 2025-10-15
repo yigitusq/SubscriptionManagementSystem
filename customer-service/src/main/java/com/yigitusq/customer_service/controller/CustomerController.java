006b8a92ad2c4ccec7aec4ad73c858646423629d
yigitusq <yigituslu077@gmail.com>
2025-10-15 18:52:32 +0300

    commit5 DtoCustomer added. AuditingEntityListener added for automatically updating the updated_at and created_at columns in database.

--- customer-service/src/main/java/com/yigitusq/customer_service/controller/CustomerController.java
+++ customer-service/src/main/java/com/yigitusq/customer_service/controller/CustomerController.java
@@ -5,7 +5,7 @@ import com.yigitusq.customer_service.dto.DtoCustomerIU;
 import com.yigitusq.customer_service.model.Customer;
 import com.yigitusq.customer_service.service.CustomerService;
 //import lombok.RequiredArgsConstructor;
-//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.http.HttpStatus;
 //import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
